70e1e9e2e9ee8f13cb25d06c3ffe19c4cb102075
dhavalpatel <root@dhaval.crest.local>
2016-03-29 17:51:01 +0530

    Mailing settings updated

--- config/environment.rb
+++ config/environment.rb
@@ -2,4 +2,13 @@
 require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
-Rails.application.initialize!
+Rails.application.initialize! 
+ActionMailer::Base.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "http://dhaval-photo-app.herokuapp.com/",
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: ENV["gmail_username"],
+  password: ENV["gmail_password"]
+}
